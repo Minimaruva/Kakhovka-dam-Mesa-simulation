f86a114c9b5a2369fac6affcc0dab0be9c86ea78
Vlad Y <vladjagnion@gmail.com>
2026-06-08 20:40:28 +0100

    completed basic mesa tutorial with working Boltzmann Wealth Model

--- learning.py
+++ learning.py
@@ -14,6 +14,7 @@ The assumption that govern this model are:
 import numpy as np
 import pandas as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 import mesa
 
 # ------- Agent ----------------------------
@@ -36,6 +37,15 @@ class MoneyAgent(mesa.Agent):
     def say_wealth(self):
         print(f"Hi, my wealth is: {self.wealth}")
 
+    def exchange(self):
+        # only exchange if weath is >0
+        if self.wealth > 0:
+            # random is same as python random but shares model seed so use it!
+            other_agent = self.random.choice(self.model.agents)
+            if other_agent is not None:
+                other_agent.wealth += 1
+                self.wealth -= 1
+
 
 # ------ Model ----------------------------
 # This is "manager" class that manages creation, activation, data collection etc. by agents
@@ -56,7 +66,11 @@ class MoneyModel(mesa.Model):
         # Advances model by single step/tick
         # This function psuedo-randomly reorders the list of agent objects and
         # then iterates through calling the function passed in as the parameter
-        self.agents.shuffle_do("say_wealth") #changing this to just 'do' makes agents ordered
+        self.agents.shuffle_do("exchange") #changing this to just 'do' makes agents ordered
+    
+    def run_for(self, steps):
+        for _ in range(steps):
+            self.step()
 
 
 # --------Agents do--------------
@@ -68,7 +82,48 @@ class MoneyModel(mesa.Model):
 # The model will run for one step and print the unique_id of each agent. 
 # You may run the model for multiple steps by calling the step method multiple times.
 
-starter_model = MoneyModel(12) # Changing seed to number makes shuffle same order!
-starter_model.step()
+# starter_model = MoneyModel(12) # Changing seed to number makes shuffle same order!
+# starter_model.step()
+
+# model = MoneyModel(10)  # Tells the model to create 10 agents
+# for _ in range(30):  # Runs the model for 30 steps;
+#     model.step()
+
+# Note: An underscore is common convention for a variable that is not used.
+
+# -------Getting and displaying the data --------
+
+# agent_wealth = [a.wealth for a in model.agents]
+# print(agent_wealth)
+# Seaborn histogram
+# g = sns.histplot(agent_wealth, discrete=True)
+# g.set(title="Wealth distribution", xlabel="Wealth", ylabel="number of agents");  # The semicolon is just to avoid printing the object representation
+
+# fixes ticks cuz of calling plt/ don't need this if run from jupyter
+# min_w = min(agent_wealth)
+# max_w = max(agent_wealth)
+# g.set_xticks(range(min_w, max_w + 1))
+
+# plt.show()
 
 
+# Even better is to run multiple models and see emergin distribution
+
+all_wealth = []
+
+for _ in range(100):
+    model = MoneyModel(10)
+    model.run_for(30)
+
+    # store results
+    for agent in model.agents:
+        all_wealth.append(agent.wealth)
+
+g = sns.histplot(all_wealth, discrete=True)
+g.set(title="Wealth distribution", xlabel="Wealth", ylabel="number of agents");
+# fixes ticks cuz of calling plt/ don't need this if run from jupyter
+min_w = min(all_wealth)
+max_w = max(all_wealth)
+g.set_xticks(range(min_w, max_w + 1))
+
+plt.show()
