15584ba7f62d5c5325309207c86158cbe99dceea
Vlad Y <vladjagnion@gmail.com>
2026-06-08 20:11:32 +0100

    wealth study agent and model

--- learning.py
+++ learning.py
@@ -1,29 +1,45 @@
 # file to practice mesa
 
+# # Boltzmann Wealth Model tutorial
+'''
+The assumption that govern this model are:
+
+- There are some number of agents.
+
+- All agents begin with 1 unit of money.
+
+- At every step of the model, an agent gives 1 unit of money (if they have it) to some other agent.
+'''
+
+import numpy as np
+import pandas as np
+import seaborn as sns
 import mesa
 
-class MyAgent(mesa.Agent):
-    def __init__(self, model, age):
+# ------- Agent ----------------------------
+# Agents have unique_id assigned at creation
+
+class MoneyAgent(mesa.Agent):
+    "Agent with fixed initial weath of 1 hryvna"
+
+    def __init__(self, model):
+        # Pass parameters to parent class
         super().__init__(model)
-        self.age = age
-
-    def step(self):
-        self.age += 1
-        print(f"Agent {self.unique_id} now is {self.age} years old")
-        # Whatever else the agent does when activated
-
-class MyModel(mesa.Model):
-    def __init__(self, n_agents):
-        super().__init__()
-        self.grid = mesa.discrete_space.OrthogonalMooreGrid((10, 10), torus=True)
-        initial_ages = self.rng.integers(0, 80, size=n_agents)
-        agents = MyAgent.create_agents(self, n_agents, initial_ages)
-        for agent in agents:
-            agent.cell = self.grid.all_cells.select_random_cell()
-
-    def step(self):
-        self.agents.shuffle_do("step")
+        # initial wealth value
+        self.wealth = 1
 
 
-model = MyModel(n_agents=5)
-model.run_for(1)
+# ------ Model ----------------------------
+# This is "manager" class that manages creation, activation, data collection etc. by agents
+# very simply it's a list containing agents (in dictionary) with discrete time steps
+
+class MoneyModle(mesa.Model):
+    "Model with n number of agents"
+
+    def __init__(self, n=10, seed=None):
+        super().__init__(seed=seed)
+        self.num_agents = n
+        # Create agents
+        MoneyAgent.create_agents(model=self, n=n)
+        # This is method from superclass Agent that handles creation of n agents with id assignment
+        # it allso auto adds them to the model self.agents!
